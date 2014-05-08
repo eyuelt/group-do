f64ed63d939164154260fe00a1949a1433df710d
eyuelt <eyueltessema@gmail.com>
2014-05-08 02:11:43 -0700

    Finish init js for board page

--- public/js/board.js
+++ public/js/board.js
@@ -1,21 +1,37 @@
-function populateEventsList() {
-  $.ajax({
-    url: '/api/event/',
-    type: 'GET',
-    success: function(response) {
-      var events = response.events;
-      var list = $('#event-list')[0];
-      for (var i = 0; i < events.length; i++) {
-        addEventToList(events[i], list);
+function init() {
+  var listDivName = '#event-list';
+
+  function populateEventsList() {
+    $.ajax({
+      url: '/api/event/',
+      type: 'GET',
+      success: function(response) {
+        var events = response.events;
+        var list = $(listDivName)[0];
+        for (var i = 0; i < events.length; i++) {
+          list.appendChild(createEventElement(events[i]));
+        }
       }
-    }
     });
-}
+  }
 
-function addEventToList(event, list) {
-  var li = document.createElement('li');
-  li.innerHTML = "Title: '<b>" + event["title"] + "</b>', Desc: '<b>" + event["description"] + "</b>'";
-  list.appendChild(li);
-}
+  function createEventElement(event) {
+    var containerDiv = document.createElement('div');
+    containerDiv.className = 'event';
+    var titleDiv = document.createElement('div');
+    titleDiv.className = 'title';
+    var descriptionDiv = document.createElement('div');
+    descriptionDiv.className = 'description';
 
-populateEventsList();
+    titleDiv.innerHTML = event['title'];
+    descriptionDiv.innerHTML = event['description'];
+
+    containerDiv.appendChild(titleDiv);
+    containerDiv.appendChild(descriptionDiv);
+    return containerDiv;
+  }
+
+  populateEventsList();
+};
+
+init();
